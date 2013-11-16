d6499610279f31ba5f62286474f5b7d30a892189
Oliver Müller <morphi@gmx.ch>
2013-11-16 12:54:31 +0100

    Zwischenstand

--- index.php
+++ index.php
@@ -1,1 +1,34 @@
-juppi! FAbingo läuft.
+<!doctype html>
+
+<html lang="de">
+    <head>
+        <title>Fabingo</title>
+        <meta name="description" content="Fabingo zum Lotto verbessern">
+        <meta name="author" content="Fabingo Team">
+        <link rel="stylesheet" type="text/css" href="/css/normalize.css">
+        <link rel="stylesheet" type="text/css" href="/css/application.css">
+    </head>
+    <body>
+
+        <div id="wrap">
+            <div id="header">
+                <div id="logo">
+                    <a href="/home"><img src="/images/logo_site.png" alt="Logo"  /></a>
+                </div>
+            </div>
+
+            <div id="menu">
+           
+            </div>
+
+            <div id="content">
+      
+            </div>
+
+            <div id="footer">
+                <p>by Oliver und Fabian</p>
+            </div>
+        </div>
+    
+    </body>
+</html>
