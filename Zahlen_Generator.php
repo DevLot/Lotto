c6a7059fa1f018ee4cb8e210c63787d44491ae31
Fabian Rüdele <fabian.ruedele@bluewin.ch>
2013-12-14 13:53:28 +0100

    Branch enthält: Zahlen_Generator, DB_Connector, Klasse Player, Klasse, Cards, Klasse Event

---
+++ Zahlen_Generator.php
@@ -0,0 +1,15 @@
+<?php
+
+$lotto[] = range(1, 89);
+shuffle($lotto);
+echo $lotto[5];
+
+?>
+
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
