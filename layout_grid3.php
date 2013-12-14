7b18c0053534d5f69273ef5cbdd7596a9997955d
Oliver Müller <morphi@gmx.ch>
2013-12-14 13:26:10 +0100

    Startseite Layout erstellt

---
+++ layout_grid3.php
@@ -0,0 +1,78 @@
+<!doctype html>
+ <meta charset="UTF-8"> 
+
+<html lang="de">
+    <head>
+        <title>Fabingo</title>
+        <meta name="description" content="Fabingo zum Lotto verbessern">
+        <meta name="author" content="Fabingo Team">
+        <link rel="stylesheet" type="text/css" href="/css/normalize.css">
+        <link rel="stylesheet" type="text/css" href="/css/application.css">
+        
+        <link href='http://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet' type='text/css'>
+    </head>
+    <body>
+
+
+        <div id="header">
+            <div class="logo">
+                <a href="/home"><img src="/images/logo_site.png" alt="Logo" height="26"/></a>
+            </div>
+                <div class="button"><a href="">Logout</a></div>
+
+        </div>
+       
+        <div id="wrap">             
+                
+                <div id="navtree">
+                    Home > Start
+                </div>
+        
+                               
+                <div id="content">
+
+                    <div class="grid">
+                        <h1>Event</h1>
+                        
+                        <ul>
+                            
+                            <li><a href="index.php">Spiel starten</a></li>
+                            <li><a href="index.php">Nächste Veranstaltungen</a></li>
+                            <li><a href="index.php">Vergangene Veranstaltungen</a></li>
+                        </ul>
+                       
+                    </div>
+
+                    <div class="grid">
+                        <h1>Eventmanagement</h1>
+                        
+                        
+                        <ul class="nav">
+                            
+                            <li><a href="index.php">Spieler hinzufügen</a></li>
+                            <li><a href="index.php">Spieler</a></li>
+                            <li><a href="index.php">Spielkarten bearbeiten</a></li>
+                         </ul>
+                    </div>
+
+                    <div class="grid">
+                        <h1>Verwaltung</h1>
+                        
+                        <ul>
+                            <li><a href="index.php">Spieler hinzufügen</a></li>
+                            <li><a href="index.php">Spieler anzeigen</a></li>
+                            <li><a href="index.php">Spielkarten bearbeiten</a></li>
+                            <li><a href="index.php">Passwort ändern</a></li>
+                        </ul>
+                            
+                    </div>
+
+                </div>
+
+                <div id="footer">
+                    by Oliver und Fabian
+                </div>
+            </div>
+
+    </body>
+</html>
