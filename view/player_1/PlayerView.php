dad8dbc1d77af2e4fe80bcf4b8af147b18c3745a
Oliver Müller <morphi@gmx.ch>
2013-12-14 18:47:58 +0100

    Player View vergessen :)

---
+++ view/player_1/PlayerView.php
@@ -0,0 +1,87 @@
+<?php
+
+class PlayerView extends View {
+
+    public function display() {
+        echo '<div class="button"><a href="/player/create">Neuer Spieler</a></div>
+                   
+
+
+<table>
+<tbody><tr>
+   <th></th>
+  <th>Vorname</th>
+  <th>Name</th>
+  <th>Geburtsdatum</th>
+  <th>Adresse</th>
+  <th>PLZ</th>
+  <th>Ort</th>
+  <th>Telefon</th>
+  <th>Mobiltelefon</th>
+  <th>Spielkarten</th>
+</tr>
+<tr>
+<td>[ ]</td>    
+<td>Oliver</td>
+<td>Müller</td>
+<td>13.7.1988</td>
+<td>Neustadt 22</td>
+<td>8200</td>
+<td>Schaffhausen</td>
+<td>0793848323</td>
+<td>9848348384</td>
+<td>32,4,3,2,4,2</td>
+</tr>
+<tr>
+    <td>[ ]</td> 
+<td>Oliver</td>
+<td>Müller</td>
+<td>13.7.1988</td>
+<td>Neustadt 22</td>
+<td>8200</td>
+<td>Schaffhausen</td>
+<td>0793848323</td>
+<td>9848348384</td>
+<td>32,4,3,2,4,2</td>
+</tr>
+<tr>
+    <td>[ ]</td> 
+<td>Oliver</td>
+<td>Müller</td>
+<td>13.7.1988</td>
+<td>Neustadt 22</td>
+<td>8200</td>
+<td>Schaffhausen</td>
+<td>0793848323</td>
+<td>9848348384</td>
+<td>32,4,3,2,4,2</td>
+</tr>
+<tr>
+    <td>[ ]</td> 
+<td>Oliver</td>
+<td>Müller</td>
+<td>13.7.1988</td>
+<td>Neustadt 22</td>
+<td>8200</td>
+<td>Schaffhausen</td>
+<td>0793848323</td>
+<td>9848348384</td>
+<td>32,4,3,2,4,2</td>
+</tr>
+<tr>
+    <td>[ ]</td> 
+<td>Oliver</td>
+<td>Müller</td>
+<td>13.7.1988</td>
+<td>Neustadt 22</td>
+<td>8200</td>
+<td>Schaffhausen</td>
+<td>0793848323</td>
+<td>9848348384</td>
+<td>32,4,3,2,4,2</td>
+</tr>
+</tbody></table>
+';
+    }
+
+}
