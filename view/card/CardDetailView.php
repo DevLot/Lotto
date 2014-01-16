42fd36c26ccde1d033264e1aa5ca53fde8759d32
Oliver Müller <morphi@gmx.ch>
2014-01-16 07:43:11 +0100

    CardDetailView erweitert.

--- view/card/CardDetailView.php
+++ view/card/CardDetailView.php
@@ -3,23 +3,11 @@
 class CardDetailView extends View {
 
     public function display() {
-        $card = $this->vars['card'];
-        
-        echo "ok";
-
-//        echo "<div class=\"event-info\">\n";
-//        echo "<h1>{$card->getName()}</h1>\n";
-//        echo "<h2>" . date("d.m.Y H:i", $event->getStarttime()) . "</h2>\n";
-//        $artist = $event->getArtist();
-//        if ($artist instanceof Artist) {
-//            echo "<p><img src=\"/resources/{$artist->getImage()}\" alt=\"{$artist->getName()}\" /></p>\n";
-//            echo "<p>{$artist->getDescription()}</p>\n";
-//
-//            if ($card->getId() == 1) {
-//                echo "ok";
-//            }
-//        }
-        echo "</div>\n";
+      $card = $this->vars['card'];
+      echo $card->getName();
+      echo "ID des Objeks:";
+      echo $card->getId();
+      
     }
 
 }
